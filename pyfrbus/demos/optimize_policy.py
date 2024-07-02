2bd94fced65084ca659760d6d9a12203eb0682ad
lequ02 <lequ02@luther.edu>
2024-07-02 11:42:36 +0700

    clean up optimize_policy

--- pyfrbus/demos/optimize_policy.py
+++ pyfrbus/demos/optimize_policy.py
@@ -3,28 +3,27 @@ from pyfrbus.frbus import Frbus
 from pyfrbus.load_data import load_data
 from pyfrbus.custom_plot import custom_plot
 
-# Load data
+# Load historical data
 data = load_data("../data/HISTDATA.TXT")
 
-# Load model
+# Load FRB/US model
 frbus = Frbus("../models/model.xml")
 
-# Specify dates
+# Define start and end periods
 start = pd.Period("2019Q4")
 end = "2023Q4"
 
-# no pandemic scenario
+# Solve for no pandemic scenario
 no_pandemic = frbus.solve(start, end, data)
 
-
-# no stay-at-home orders, foreign activity and expectations match the historical data
+# Create no stay-at-home orders scenario
 no_stayhome_data = no_pandemic.copy(deep=True)
-variables = pd.read_csv("model_variables_simple.csv")
-const_variables = variables[(variables["sector"]=="Foreign Activity") | (variables["sector"]=="Expectations")].name
 
-targ_no_stayhome = []
-traj_no_stayhome = []
-inst_no_stayhome = []
+# Load model variables
+variables = pd.read_csv("model_variables_simple.csv")
+const_variables = variables[(variables["sector"] == "Foreign Activity") | (variables["sector"] == "Expectations")].name
+
+targ_no_stayhome, traj_no_stayhome, inst_no_stayhome = [], [], []
 
 for name in const_variables:
     try:
@@ -33,75 +32,104 @@ for name in const_variables:
         traj_no_stayhome.append(f"{name}_t")
         inst_no_stayhome.append(name)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        print(f"An error occurred with variable {name}: {e}")
 
-stay_home_total = 17 # num. weeks stay-at-home orders were in effect for all America
+# Adjust unemployment rates for stay-at-home orders
+stay_home_total = 17  # weeks
 start_stayhome = pd.Period("2020Q2")
 end_stayhome = pd.Period("2020Q2")
-no_stayhome_data.loc[start_stayhome:end_stayhome, "lurnat_t"] = data.loc[start_stayhome:end_stayhome, 'lurnat'] * (1-.019)**stay_home_total      # natural rate of unemployment rate
-no_stayhome_data.loc[start_stayhome:end_stayhome, "lur_t"] = data.loc[start_stayhome:end_stayhome, 'lur'] * (1-.019)**stay_home_total      # rate of unemployment rate
 
-no_stayhome_data.loc[end_stayhome+1:end, "lurnat_t"] = data.loc[end_stayhome+1:end, 'lurnat']
-no_stayhome_data.loc[end_stayhome+1:end, "lur_t"] = data.loc[end_stayhome+1:end, 'lur']
+no_stayhome_data.loc[start_stayhome:end_stayhome, "lurnat_t"] = data.loc[start_stayhome:end_stayhome, 'lurnat'] * (1 - .019)**stay_home_total
+no_stayhome_data.loc[start_stayhome:end_stayhome, "lur_t"] = data.loc[start_stayhome:end_stayhome, 'lur'] * (1 - .019)**stay_home_total
 
-# Define the target variables and their trajectories
-targ_no_stayhome +=['lur', 'lurnat']
-traj_no_stayhome +=['lur_t', 'lurnat_t']
-inst_no_stayhome +=['lur', 'lurnat']
+no_stayhome_data.loc[end_stayhome + 1:end, "lurnat_t"] = data.loc[end_stayhome + 1:end, 'lurnat']
+no_stayhome_data.loc[end_stayhome + 1:end, "lur_t"] = data.loc[end_stayhome + 1:end, 'lur']
+
+# Update target and trajectory lists
+targ_no_stayhome += ['lur', 'lurnat']
+traj_no_stayhome += ['lur_t', 'lurnat_t']
+inst_no_stayhome += ['lur', 'lurnat']
 
 # Run mcontrol to match the target variables to their trajectories
 no_stayhome = frbus.mcontrol(start, end, no_stayhome_data, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
 
+# Adjust for anticipated errors during stay-at-home period
+stayhome_aerr_data = no_stayhome.copy(deep=True)
+
+stayhome_aerr_data.loc[start_stayhome:end_stayhome, "lur_t"] = stayhome_aerr_data.loc[start_stayhome:end_stayhome, 'lur'] * (1.019**stay_home_total)
+stayhome_aerr_data.loc[start_stayhome:end_stayhome, "lurnat_t"] = stayhome_aerr_data.loc[start_stayhome:end_stayhome, 'lurnat'] * (1.019**stay_home_total)
+
+stayhome_aerr = frbus.mcontrol(start_stayhome, end_stayhome, stayhome_aerr_data, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
+stayhome_aerr = frbus.solve(end_stayhome, end, stayhome_aerr)
+stayhome_aerr = frbus.mcontrol(end_stayhome, end, stayhome_aerr, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
+
+# Calculate errors for each variable
+for name in variables['name']:
+    try:
+        stayhome_aerr.loc[start:end, f"{name}_aerr"] = data.loc[start:end, name] - stayhome_aerr.loc[start:end, name]
+    except Exception as e:
+        print(f"An error occurred with variable {name}: {e}")
 
 
-# custom stay-at-home order
 
-targ_custom = targ_no_stayhome.copy()
-traj_custom = traj_no_stayhome.copy()
-inst_custom = inst_no_stayhome.copy()
+# Custom stay-at-home order scenario
+targ_custom, traj_custom, inst_custom = targ_no_stayhome.copy(), traj_no_stayhome.copy(), inst_no_stayhome.copy()
 
 start_lockdown_opt = pd.Period("2020Q2")
-end_lockdown_opt = pd.Period("2020Q2")
+end_lockdown_opt = pd.Period("2020Q3")
+custom_lockdown_duration = 30  # weeks
 
-
-lockdown_duration = 17 # duration in weeks
 custom_stayhome_data = no_stayhome.copy(deep=True)
 
-custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, "lur_t"] = custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, 'lur'] * (1.019**lockdown_duration)
-custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, "lurnat_t"] = custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, 'lurnat'] * (1.019**lockdown_duration)
+custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, "lur_t"] = custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, 'lur'] * (1.019**custom_lockdown_duration)
+custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, "lurnat_t"] = custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, 'lurnat'] * (1.019**custom_lockdown_duration)
 
-# print(custom_stayhome_data.loc[start_lockdown_opt:end, 'lurnat_t'])
-# print(custom_stayhome_data.loc[start_lockdown_opt:end, 'lur_t'])
-
-# print(custom_stayhome_data.loc[end_lockdown_opt:end, 'lur_t'])
-
-# print('data')
-# print(custom_stayhome.loc[end_lockdown_opt:end, 'lurnat_t'])
-custom_stayhome = frbus.mcontrol(start_lockdown_opt, end_lockdown_opt, custom_stayhome_data, targ_custom, traj_custom, inst_custom) 
+custom_stayhome = frbus.mcontrol(start_lockdown_opt, end_lockdown_opt, custom_stayhome_data, targ_custom, traj_custom, inst_custom)
 custom_stayhome = frbus.solve(end_lockdown_opt, end, custom_stayhome)
 custom_stayhome = frbus.mcontrol(end_lockdown_opt, end, custom_stayhome, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
 
-no_stayhome.to_csv("./results/optimize/no_stayhome.csv", index=True)
-custom_stayhome.to_csv("./results/optimize/custom_stayhome.csv", index=True)
+# Apply anticipated errors
+for name in variables['name']:
+    try:
+        custom_stayhome[name] += stayhome_aerr[f"{name}_aerr"]
+    except Exception as e:
+        print(f"An error occurred with variable {name}: {e}")
 
+# Save results
+no_stayhome.to_csv("./results/external_const/no_stayhome.csv", index=True)
+custom_stayhome.to_csv("./results/external_const/custom_stayhome.csv", index=True)
 
-
-# foreign_const_sim.to_csv("mcontrol.csv", index=True)
-
-# # # View results
-variables = pd.read_csv('model_variables_simple.csv')
-
-
+# Generate plots
 dfs = {'baseline': data, 'custom_stayhome': custom_stayhome, 'no_stayhome': no_stayhome, 'no_pandemic': no_pandemic}
 
-plots = [{'column': 'xgdp', 'type': 'pct_change'}, {'column': 'pcxfe', 'type': 'pct_change', 'name':'PCE Price Index'}, {'column': 'xgdpt'}, {'column': 'pcpi', 'type':'pct_change', 'name':'CPI'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/gdp+inflation.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'xgdp', 'type': 'pct_change'},
+    {'column': 'pcxfe', 'type': 'pct_change', 'name': 'PCE Price Index'},
+    {'column': 'xgdpt'},
+    {'column': 'pcpi', 'type': 'pct_change', 'name': 'CPI'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/external_const/gdp+inflation.png', plot_title='No April lockdown + same foreign variables')
 
-plots = [{'column': 'lprdt'}, {'column': 'lhp', 'name':'Agg. labor hours, business sector'}, {'column': 'lww', 'name':'Workweek, business sector'}, {'column': 'lqualt'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/labor quality.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'lprdt'},
+    {'column': 'lhp', 'name': 'Agg. labor hours, business sector'},
+    {'column': 'lww', 'name': 'Workweek, business sector'},
+    {'column': 'lqualt'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/external_const/labor quality.png', plot_title='No April lockdown + same foreign variables')
 
-plots = [{'column': 'leg', 'name': 'Gov. civilian employment'}, {'column': 'lur', 'name':'Unemployment Rate'}, {'column': 'lurnat'}, {'column': 'leh'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/(un)employment.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'leg', 'name': 'Gov. civilian employment'},
+    {'column': 'lur', 'name': 'Unemployment Rate'},
+    {'column': 'lurnat'},
+    {'column': 'leh'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/external_const/(un)employment.png', plot_title='No April lockdown + same foreign variables')
 
-plots = [{'column': 'ex', 'name':'Export, cw 2012$'}, {'column': 'em','name':'Import, cw 2012$' }, {'column': 'fpic', 'name': 'Foreign CPI (G39)'}, {'column': 'fgdp', 'name':'Foreign GDP (G39)'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/export+import.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'ex', 'name': 'Export, cw 2012$'},
+    {'column': 'em', 'name': 'Import, cw 2012$'},
+    {'column': 'fpic', 'name': 'Foreign CPI (G39)'},
+    {'column': 'fgdp', 'name': 'Foreign GDP (G39)'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/external_const/export+import.png', plot_title='No April lockdown + same foreign variables')
--- pyfrbus/demos/optimize_policy2.py
+++ pyfrbus/demos/optimize_policy2.py
@@ -2,123 +2,150 @@ import pandas as pd
 from pyfrbus.frbus import Frbus
 from pyfrbus.load_data import load_data
 from pyfrbus.custom_plot import custom_plot
+import numpy as np
 
-# Load data
+# Load historical data
 data = load_data("../data/HISTDATA.TXT")
 
-# Load model
+# Load FRB/US model
 frbus = Frbus("../models/model.xml")
 
-# Specify dates
+# Define start and end periods
 start = pd.Period("2019Q4")
 end = "2023Q4"
 
+# Solve for no pandemic scenario
 no_pandemic = frbus.solve(start, end, data)
-print(no_pandemic.shape)
-print(no_pandemic["lur_trac"].values)
-# Set up trajectories to match the historical data
-# no_pandemic.loc[start:end, "fgdp_t"] = data.loc[start:end, "fgdp"]     # foreign GDP (world)
-# no_pandemic.loc[start:end, "fgdpt_t"] = data.loc[start:end, "fgdpt"]   # foreign GDP trend (world)
-# no_pandemic.loc[start:end, "fpic_t"] = data.loc[start:end, "fpic"]     # foreign CPI (bilateral export trade weights)
+
+# Create no stay-at-home orders scenario
+no_stayhome_data = no_pandemic.copy(deep=True)
+no_stayhome_data = frbus.init_trac(start, end, no_stayhome_data)
+
+# Load model variables
+variables = pd.read_csv("model_variables_simple.csv")
+dynamic_variables = variables[(variables["sector"] == "Labor Market") | (variables["sector"] == "Household Expenditures")].name
+
+# print(changing_variables)
+
+targ_no_stayhome, traj_no_stayhome, inst_no_stayhome = [], [], []
+
+for name in dynamic_variables:
+    try:
+        # no_stayhome_data.loc[start:end, f"{name}_t"] = data.loc[start:end, name]
+        # targ_no_stayhome.append(name)
+        # traj_no_stayhome.append(f"{name}_t")
+        # inst_no_stayhome.append(name)
+        no_stayhome_data[f"{name}_trac"] = np.zeros(len(no_stayhome_data))
+
+    except Exception as e:
+        print(f"An error occurred with variable {name}: {e}")
 
 
-# no_pandemic.loc[start:end, "fpc_t"] = data.loc[start:end, 'fpc']       # foreign aggregate consumer price (G39)
-# no_pandemic.loc[start:end, "frl10_t"] = data.loc[start:end, 'frl10']   # foreign long-term interest rate (G10)
-# no_pandemic.loc[start:end, "frs10_t"] = data.loc[start:end, 'frs10']   # foreign short-term interest rate (G10)
+print(set(no_stayhome_data['ec_trac'].values))
+print((no_stayhome_data['xgdp'].values))
+print(len(no_stayhome_data['xgdp'].values))
+print(len(no_stayhome_data['xgdpt'].values))
 
 
-no_stayhome_data = frbus.init_trac(start, end, data)
-print(no_stayhome_data.shape)
-print(no_stayhome_data["lur_trac"].values)
-
-# no_stayhome_data.drop(['lurnat_aerr', 'lur_aerr'], axis=1, inplace=True)
-stay_home_total = 17 # weeks
+# Adjust unemployment rates for stay-at-home orders
+stay_home_total = 17  # weeks
 start_stayhome = pd.Period("2020Q2")
-end_stayhome = pd.Period("2020Q3")
-# no_stayhome_data.loc[start_stayhome:end_stayhome, "lurnat_t"] = data.loc[start_stayhome:end_stayhome, 'lurnat'] * (1-.019)**stay_home_total      # natural rate of unemployment rate
-no_stayhome_data.loc[start_stayhome:end_stayhome, "lur_t"] = data.loc[start_stayhome:end_stayhome, 'lur'] * (1-.019)**stay_home_total      # rate of unemployment rate
+end_stayhome = pd.Period("2020Q2")
 
-# no_stayhome_data.loc[end_stayhome+1:end, "lurnat_t"] = data.loc[end_stayhome+1:end, 'lurnat']
-no_stayhome_data.loc[end_stayhome+1:end, "lur_t"] = data.loc[end_stayhome+1:end, 'lur']
+no_stayhome_data.loc[start_stayhome:end_stayhome, "lurnat_trac"] = data.loc[start_stayhome:end_stayhome, 'lurnat'] * (1 - .019)**stay_home_total
+no_stayhome_data.loc[start_stayhome:end_stayhome, "lur_trac"] = data.loc[start_stayhome:end_stayhome, 'lur'] * (1 - .019)**stay_home_total
 
-# print("trac", no_stayhome_data.loc[start_stayhome:end_stayhome, 'lurnat_t'])
-# print("data", no_stayhome_data.loc[start_stayhome:end_stayhome, 'lurnat'])
+no_stayhome_data.loc[end_stayhome + 1:end, "lurnat_trac"] = data.loc[end_stayhome + 1:end, 'lurnat']
+no_stayhome_data.loc[end_stayhome + 1:end, "lur_trac"] = data.loc[end_stayhome + 1:end, 'lur']
 
-# Define the target variables and their trajectories
-# targ_no_stayhome = ["fgdp", "fgdpt", "fpic", "fpc", "frl10", "frs10", 'lur', 'lurnat']
-# traj_no_stayhome = ["fgdp_t", "fgdpt_t", "fpic_t", "fpc_t", "frl10_t", "frs10_t", 'lur_t', 'lurnat_t']
-
-targ_no_stayhome = ['lur']
-traj_no_stayhome = ['lur_t']
-# Define instruments as add-factors for flexibility
-# inst_no_stayhome = ["fgdp", "fgdpt", "fpic", "fpc", "frl10", "frs10", 'lur', 'lurnat']
-inst_no_stayhome = ['lur']
+print(set(no_stayhome_data['lurnat_trac'].values))
+print(set(no_stayhome_data['lur_trac'].values))
+# Update target and trajectory lists
+# targ_no_stayhome += ['lur', 'lurnat']
+# traj_no_stayhome += ['lur_t', 'lurnat_t']
+# inst_no_stayhome += ['lur', 'lurnat']
 
 # Run mcontrol to match the target variables to their trajectories
+# no_stayhome = frbus.mcontrol(start, end, no_stayhome_data, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
+no_stayhome = frbus.solve(start, end, no_stayhome_data)
 
-no_stayhome = frbus.mcontrol(start, end, no_stayhome_data, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
-# print("last", no_stayhome.loc[start_stayhome:end_stayhome, 'lurnat'])
+# Adjust for anticipated errors during stay-at-home period
+stayhome_aerr_data = no_stayhome.copy(deep=True)
 
-# ec
-# ecd
-# ech
-# ecnia
-# ecnian
-# eco
+stayhome_aerr_data.loc[start_stayhome:end_stayhome, "lur_t"] = stayhome_aerr_data.loc[start_stayhome:end_stayhome, 'lur'] * (1.019**stay_home_total)
+stayhome_aerr_data.loc[start_stayhome:end_stayhome, "lurnat_t"] = stayhome_aerr_data.loc[start_stayhome:end_stayhome, 'lurnat'] * (1.019**stay_home_total)
 
-# em
-# emn
-# emo
-# emon
-# emp
-# empn
-# emptrt
-# ex
-# exn
-# fcbn
-# fcbrn
+stayhome_aerr = frbus.mcontrol(start_stayhome, end_stayhome, stayhome_aerr_data, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
+stayhome_aerr = frbus.solve(end_stayhome, end, stayhome_aerr)
+stayhome_aerr = frbus.mcontrol(end_stayhome, end, stayhome_aerr, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
 
-# hggdp
-# hggdpt
-
-#n16
+# Calculate errors for each variable
+for name in variables['name']:
+    try:
+        stayhome_aerr.loc[start:end, f"{name}_aerr"] = data.loc[start:end, name] - stayhome_aerr.loc[start:end, name]
+    except Exception as e:
+        print(f"An error occurred with variable {name}: {e}")
 
 
-targ_custom = ['lur']
-traj_custom = ["lur_t"]
-inst_custom = ['lur']
+
+# Custom stay-at-home order scenario
+targ_custom, traj_custom, inst_custom = targ_no_stayhome.copy(), traj_no_stayhome.copy(), inst_no_stayhome.copy()
 
 start_lockdown_opt = pd.Period("2020Q2")
-end_lockdown_opt = pd.Period("2020Q3")
+end_lockdown_opt = pd.Period("2020Q2")
+custom_lockdown_duration = 17  # weeks
 
+custom_stayhome_data = no_stayhome.copy(deep=True)
 
-lockdown_duration = 17 # duration in weeks
-no_stayhome.loc[start_lockdown_opt:end_lockdown_opt, "lur_t"] = no_stayhome.loc[start_lockdown_opt:end_lockdown_opt, 'lur'] * 1.019**lockdown_duration 
+custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, "lur_t"] = custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, 'lur'] * (1.019**custom_lockdown_duration)
+custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, "lurnat_t"] = custom_stayhome_data.loc[start_lockdown_opt:end_lockdown_opt, 'lurnat'] * (1.019**custom_lockdown_duration)
 
-# no_stayhome.loc[start_lockdown_opt:end_lockdown_opt, "lurnat_t"] = no_stayhome.loc[start_lockdown_opt:end_lockdown_opt, 'lurnat'] * 1.019**lockdown_duration
-custom_stayhome = frbus.mcontrol(start_lockdown_opt, end_lockdown_opt, no_stayhome, targ_custom, traj_custom, inst_custom)
+custom_stayhome = frbus.mcontrol(start_lockdown_opt, end_lockdown_opt, custom_stayhome_data, targ_custom, traj_custom, inst_custom)
 custom_stayhome = frbus.solve(end_lockdown_opt, end, custom_stayhome)
 custom_stayhome = frbus.mcontrol(end_lockdown_opt, end, custom_stayhome, targ_no_stayhome, traj_no_stayhome, inst_no_stayhome)
 
-no_stayhome.to_csv("./results/optimize/no_stayhome.csv", index=True)
-custom_stayhome.to_csv("./results/optimize/custom_stayhome.csv", index=True)
+# Apply anticipated errors
+for name in variables['name']:
+    try:
+        custom_stayhome[name] += stayhome_aerr[f"{name}_aerr"]
+    except Exception as e:
+        print(f"An error occurred with variable {name}: {e}")
 
+# Save results
+no_stayhome.to_csv("./results/dynamic_labor_consumption/no_stayhome.csv", index=True)
+custom_stayhome.to_csv("./results/dynamic_labor_consumption/custom_stayhome.csv", index=True)
 
-
-# # # View results
-variables = pd.read_csv('model_variables_simple.csv')
-
-
+# Generate plots
 dfs = {'baseline': data, 'custom_stayhome': custom_stayhome, 'no_stayhome': no_stayhome, 'no_pandemic': no_pandemic}
 
-plots = [{'column': 'xgdp', 'type': 'pct_change'}, {'column': 'pcxfe', 'type': 'pct_change', 'name':'PCE Price Index'}, {'column': 'xgdpt'}, {'column': 'pcpi', 'type':'pct_change', 'name':'CPI'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/gdp+inflation.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'xgdp', 'type': 'pct_change'},
+    {'column': 'pcxfe', 'type': 'pct_change', 'name': 'PCE Price Index'},
+    {'column': 'xgdpt'},
+    {'column': 'pcpi', 'type': 'pct_change', 'name': 'CPI'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/dynamic_labor_consumption/gdp+inflation.png', plot_title='No April lockdown + same foreign variables')
 
-plots = [{'column': 'lprdt'}, {'column': 'lhp', 'name':'Agg. labor hours, business sector'}, {'column': 'lww', 'name':'Workweek, business sector'}, {'column': 'lqualt'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/labor quality.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'lprdt'},
+    {'column': 'lhp', 'name': 'Agg. labor hours, business sector'},
+    {'column': 'lww', 'name': 'Workweek, business sector'},
+    {'column': 'lqualt'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/dynamic_labor_consumption/labor quality.png', plot_title='No April lockdown + same foreign variables')
 
-plots = [{'column': 'leg', 'name': 'Gov. civilian employment'}, {'column': 'lur', 'name':'Unemployment Rate'}, {'column': 'lurnat'}, {'column': 'leh'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/(un)employment.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'leg', 'name': 'Gov. civilian employment'},
+    {'column': 'lur', 'name': 'Unemployment Rate'},
+    {'column': 'lurnat'},
+    {'column': 'leh'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/dynamic_labor_consumption/(un)employment.png', plot_title='No April lockdown + same foreign variables')
 
-plots = [{'column': 'ex', 'name':'Export, cw 2012$'}, {'column': 'em','name':'Import, cw 2012$' }, {'column': 'fpic', 'name': 'Foreign CPI (G39)'}, {'column': 'fgdp', 'name':'Foreign GDP (G39)'}]
-custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/optimize/export+import.png', plot_title='No April lockdown + same foreign variables')
+plots = [
+    {'column': 'ex', 'name': 'Export, cw 2012$'},
+    {'column': 'em', 'name': 'Import, cw 2012$'},
+    {'column': 'fpic', 'name': 'Foreign CPI (G39)'},
+    {'column': 'fgdp', 'name': 'Foreign GDP (G39)'}
+]
+custom_plot(dfs, '2020Q1', '2023Q4', plots, variables, './results/dynamic_labor_consumption/export+import.png', plot_title='No April lockdown + same foreign variables')
